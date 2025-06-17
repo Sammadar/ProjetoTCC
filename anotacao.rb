db43c2b50f7b5a7eea3a4dd91915fad89f473930
Sammadar dos Santos <75789@proway.treina>
2025-06-16 21:43:41 -0300

    first commit

---
+++ anotacao.rb
@@ -0,0 +1,55 @@
+home
+    carousel = circular
+    dataView - layout
+login
+    e-mail
+    senha
+    cadastrar
+
+tela de cadatro
+dados do usuário
+    nome
+    cpf
+    e-mail
+    senha
+
+Carrinho
+dataView
+quantidade
+listar produtos permitindo alterar a quantidade ou excluir
+                permitindo alterar a quantidade, recalcular o preço do produto e junto com o total do pedido
+                remover o produto, recalculando o total do pedido
+
+Calcular frete
+se usuário possui cadastro, 
+endereço
+novo endereço
+
+pagamento
+forma de pagamento
+stripe
+
+negociação
+vai para o Whatsapp
+quantidade solicitada for de valor maior, sugerir negociação
+
+
+
+tela revisão do pedido
+
+tela de lista de pedidos 
+
+tela de pedido realizados com sucesso
+
+hospedagem 
+
+hostinger 
+registrobr
+godaddy
+
+telas de admin
+
+        tela de lista de pedidos
+        tela de detalhe do pedido
+        tela de lista de produtos
+        tela de cadastro de produto
