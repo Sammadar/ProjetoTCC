d8041bb31d0f869e9f502294037d336c83356c01
Sammadar dos Santos <sammadar@outlook.com.br>
2025-06-18 20:57:13 -0300

    Criado projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'projeto-tcc';
+}
---
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Menubar } from 'primeng/menubar';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [Menubar, ButtonModule],
+  templateUrl: './navbar.component.html',
+  standalone: true,
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  items: MenuItem [];
+
+  constructor(private router: Router) {
+    this.items = [
+      {
+        label: 'Menu',
+        icon: 'pi-pi-home'
+      },
+      {
+        label: 'Brigadeiros'
+      },
+      {
+        label: 'Pão de mel'
+      },
+      {
+        label: 'Bolacha'
+      },
+      {
+        label: 'Bolos'
+      },
+      {
+        label: 'Doces gourmet'
+      },
+      {
+        label: 'Combos'
+      },
+    ]
+  }
+
+  private navegar(caminho: string){
+    this.router.navigate([caminho]);
+  }
+
+}
